663d83c06adc4b4361863bfe9ebad51d3a896945
Robert Diamond <rmd6502@gmail.com>
2013-07-21 17:17:25 -0400

    Initial Commit

---
+++ example3/com_robertdiamondAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  com_robertdiamondAppDelegate.h
+//  example3
+//
+//  Created by Robert Diamond on 7/21/13.
+//  Copyright (c) 2013 Robert Diamond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class com_robertdiamondViewController;
+
+@interface com_robertdiamondAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) com_robertdiamondViewController *viewController;
+
+@end
---
+++ example3/com_robertdiamondViewController.h
@@ -0,0 +1,13 @@
+//
+//  com_robertdiamondViewController.h
+//  example3
+//
+//  Created by Robert Diamond on 7/21/13.
+//  Copyright (c) 2013 Robert Diamond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_robertdiamondViewController : UIViewController
+
+@end
